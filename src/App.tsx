93027a1816b80b2961341c276f7058d2d844f6e4
Michael Palmer <palmer.mikepalmer@gmail.com>
2023-03-18 02:01:27 -0500

    add tiedye Sidenote, switch to flexbox design

--- src/App.tsx
+++ src/App.tsx
@@ -9,38 +9,37 @@ function App() {
         <div className='App-title'>Components</div>
       </header>
       <div className='App-body'>
-        {/* TODO / Early gameplan/pseudo code for how to display each component
-         A grid that has a small clickable image of the component
-         When clicked on: navigates to an example of component with code that can be copied
-        */}
 
         {/* SIDENOTE COMPONENT */}
         <section className='component-section'>
-          <div className='component-title'>Sidenote</div>
+          <div className='component-title'>{`<Sidenote />`}</div>
           <div className='component-wrapper'>
-            <Sidenote type="notice" title="Sidenote Notice...">
+            <Sidenote type='notice' title='Sidenote Notice...'>
               <p>Hello World</p>
             </Sidenote>
-            <Sidenote type="success" title="Sidenote Success!">
+            <Sidenote type='success' title='Sidenote Success!'>
               <p>Muchness of success</p>
             </Sidenote>
-            <Sidenote type="warning" title="This is a warning!">
+            <Sidenote type='warning' title='This is a warning!'>
               <>
                 <p><strong id='never-say-never'>Never Say Never</strong></p>
                 <p>In this example, we're using lyrics to elevate our <strong>wellbeing!</strong></p>
               </>
             </Sidenote>
-            <Sidenote type="error" title="Error occurred!">
+            <Sidenote type='error' title='Error occurred!'>
               <p>Yay reusability!</p>
+            </Sidenote>
+            <Sidenote type='tiedye' title='oh em gee itz tiedye!'>
+              <p>Yay tiedye!</p>
             </Sidenote>
           </div>
         </section>
 
         {/* FUTURE COMPONENT */}
         <section className='component-section'>
-          <div className='component-title'>Future</div>
+          <div className='component-title'>{`<Future />`}</div>
           <div className='component-wrapper'>
-            <p style={{ color: 'aliceblue' }}>Placeholder for <code>{'<FutureComponent/>'}</code> example section</p>
+            <p style={{ color: 'aliceblue' }}>Placeholder for <code style= {{ color: 'mediumspringgreen' }}>{'<FutureComponent/>'}</code> example section</p>
           </div>
         </section>
       </div>
--- src/components/Sidenote.tsx
+++ src/components/Sidenote.tsx
@@ -1,4 +1,5 @@
 import './Sidenote.css';
+import { CheckCircle, Info, AlertTriangle, Gift } from 'react-feather';
 
 type SidenoteProps = {
   type: string;
@@ -6,10 +7,23 @@ type SidenoteProps = {
   children: React.ReactNode;
 };
 
+const plzGiveSidenoteIcon: any = {
+  'notice':  <div className='sidenote-icon'><Info size={32} className='sidenote-icon-notice' /></div>,
+  'warning':  <div className='sidenote-icon'><AlertTriangle size={32} className='sidenote-icon-warning'/></div>,
+  'error':  <div className='sidenote-icon'><AlertTriangle size={32} className='sidenote-icon-error'/></div>,
+  'success':  <div className='sidenote-icon'><CheckCircle  size={32} className='sidenote-icon-success'/></div>,
+  'tiedye':  <div className='sidenote-icon'><Gift  size={32} className='sidenote-icon-gift'/></div>,
+};
+
 const Sidenote = ({ type, title, children }: SidenoteProps) => (
-  <aside className={`wrapper ${type}`} /*draggable*/>
-    <h3 className="title">{title}</h3>
-    <>{children}</>
+  <aside className={`wrapper ${type}`} id={type === 'tiedye' ? 'never-say-never' : ''}/*draggable*/>
+    <div className='left'>
+      {type ? plzGiveSidenoteIcon[type] : null}
+    </div>
+    <div className='right'>
+      <div className="title">{title}</div>
+      <>{children}</>
+    </div>
   </aside>
 );
 
